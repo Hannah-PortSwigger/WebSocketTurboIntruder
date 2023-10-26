0fe99ab379bf1e4d315e48181e0e2305764b260f
Hannah Law <hannah.law@portswigger.net>
2023-10-26 11:57:14 +0100

    Remove support for ScriptEnvironment.py.

--- src/main/java/attack/AttackHandler.java
+++ src/main/java/attack/AttackHandler.java
@@ -16,44 +16,19 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class AttackHandler
 {
-    private final MontoyaApi api;
-    private final AtomicBoolean isProcessing;
-    private final BlockingQueue<ConnectionMessage> tableBlockingQueue;
-    private final WebSocketMessage baseWebSocketMessage;
     private final PythonInterpreter interpreter;
-//    private final String scriptEnvironment;
 
     public AttackHandler(MontoyaApi api, AtomicBoolean isProcessing, BlockingQueue<WebSocketConnectionMessage> sendMessageQueue, BlockingQueue<ConnectionMessage> tableBlockingQueue, WebSocketMessage baseWebSocketMessage)
     {
-        this.api = api;
-        this.isProcessing = isProcessing;
-        this.tableBlockingQueue = tableBlockingQueue;
-        this.baseWebSocketMessage = baseWebSocketMessage;
-
         interpreter = new PythonInterpreter();
         interpreter.setOut(api.logging().output());
         interpreter.setErr(api.logging().error());
-
-//        String data = null;
-//        try (InputStream stream = WebSocketEditorPanel.class.getResourceAsStream("/ScriptEnvironment.py"))
-//        {
-//            if (stream != null)
-//            {
-//                data = IOUtils.toString(stream);
-//            }
-//        } catch (IOException e)
-//        {
-//            throw new RuntimeException(e);
-//        }
-//        scriptEnvironment = data;
 
         interpreter.set("base_websocket", baseWebSocketMessage);
 
         interpreter.set("websocket_connection", new ConnectionFactory(api, isProcessing, this, sendMessageQueue));
 
         interpreter.set("results_table", new TableBlockingQueueProducer(api, tableBlockingQueue));
-
-//        interpreter.exec(scriptEnvironment);
     }
 
     public void executeJython(String payload, String editorCodeString)
