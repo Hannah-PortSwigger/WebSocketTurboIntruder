041ce4225c7a2501123669dc47e5eb5667d0dcfb
Hannah Law <hannah.law@portswigger.net>
2024-08-16 16:49:16 +0100

    Update examples with comment.

--- src/main/resources/examples/BasicExampleWithComments.py
+++ src/main/resources/examples/BasicExampleWithComments.py
@@ -2,7 +2,7 @@ def queue_websockets(upgrade_request, message):
     connection1 = websocket_connection.create(upgrade_request)
 
     for i in range(10):
-        connection1.queue(message, "foo")
+        connection1.queueWithComment(message, "foo")
 
 def handle_outgoing_message(websocket_message):
     results_table.add(websocket_message)
